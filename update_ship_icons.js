3a1b2d9811f1e42fb4b2bbf735b45485e8bb9c60
SIH Hacker <sih-hacker@example.com>
2026-09-13 12:18:01 +0530

    chore: remove old scripts causing eslint errors

--- update_ship_icons.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let mapCode = fs.readFileSync('src/components/GlobeWrapper.tsx', 'utf8');
-
-mapCode = mapCode.replace(/color=\{isSelected \? "\#ffffff" : isHovered \? "var\(--route-active\)" : "\#aaaaaa"\}/g, 'color={isSelected ? "var(--map-text)" : isHovered ? "var(--route-active)" : "var(--map-stroke)"}');
-
-fs.writeFileSync('src/components/GlobeWrapper.tsx', mapCode);
-console.log("Updated ship icon colors");
